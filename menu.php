8a541ccf2bce61946b4058e3c89a49f477a41f20
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-08-15 22:32:43 +0200

    WIP: update menu.css \ menu.tsv \ menu.php

--- menu.php
+++ menu.php
@@ -1,8 +1,30 @@
 <?php
+require_once 'functions.php';
 $title = "Our menu";
+$lignes = file(__DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'menu.tsv');
+foreach ($lignes as $k => $ligne) {
+    $lignes[$k] = explode("\t", trim($ligne));
+}
 require 'elements/header.php';
 ?>
 
 <h1>Menu</h1>
 
+<?php foreach($lignes as $ligne): ?>
+    <?php if (count($ligne) === 1): ?>
+        <h2><?= $ligne[0] ?></h2>
+    <?php else: ?>
+        <div class="row">
+            <div class="col-sm-8">
+                <p>
+                    <strong><?= $ligne[0]; ?></strong>
+                    <?= $ligne[1]; ?>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <strong><?= number_format($ligne[2], 2, ',', ' ') ?> € </strong>
+            </div>
+        </div>
+    <?php endif; ?>
+<?php endforeach; ?>
 <?php require 'elements/footer.php'; ?>
