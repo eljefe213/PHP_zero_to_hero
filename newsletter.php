858292366f11b255116bd2ec8d2bf733e727e291
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-08-16 18:30:32 +0200

    WIP: update newsletter.php

--- newsletter.php
+++ newsletter.php
@@ -1,9 +1,24 @@
 <?php
+$error = null;
+$email = null;
+if (!empty($_POST['email'])) {
+    $email = $_POST['email'];
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    } else {
+        $error = "Invalid email !!";
+    }
+}
 require 'elements/header.php';
 ?>
 
-<form action="/newsletter.php" method="post">
+<h1>Register to the newsletter</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus deleniti esse, mollitia debitis impedit eius et accusantium tenetur assumenda voluptate cum commodi reprehenderit praesentium. Sed voluptatum fugit enim excepturi laudantium.</p>
 
+<form action="/newsletter.php" method="post" class="form-inline">
+    <div class="form-group">
+        <input type="email" name="email" placeholder="Entre your email" class="form-control" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Register</button>
 </form>
 
 <?php require 'elements/footer.php'; ?>
