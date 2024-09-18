31b5ee72d09e3bb6efb78418d020361c8e112fce
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-09-18 19:46:11 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,13 +2,30 @@
 require 'functions/counter.php';
 $total = number_view();
 $year = (int)date('Y');
+$selected_year = empty($_GET['year']) ? $year : (int)$_GET['year'];
+$month = [
+    '01' => 'January',
+    '02' => 'February',
+    '03' => 'March',
+    '04' => 'April',
+    '05' => 'May',
+    '06' => 'June',
+    '07' => 'July',
+    '08' => 'August',
+    '09' => 'September',
+    '10' => 'October',
+    '11' => 'November',
+    '12' => 'December'
+];
 require 'elements/header.php';
 ?>
 <div class="row">
     <div class="col-md-4">
-        <ul class="list-group">
-
-        </ul>
+        <div class="list-group">
+            <?php for ($i = 0; $i < 5; $i++): ?>
+                <a class="list-group-item <?= $year - $i === $selected_year ? 'active' : ''; ?>" href="dashboard.php?year=<?= $year - $i ?>"><?= $year - $i ?></a>
+            <?php endfor ?>
+        </div>
     </div>
     <div class="col-md-8">
         <div class="card">
