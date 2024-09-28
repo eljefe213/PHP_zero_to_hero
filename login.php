0b543e0d72a93931565a5cdf27668fd8cfb7c9c4
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-09-28 07:56:28 +0200

    WIP: Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,21 @@
 <?php
+$error = null;
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
+    if ($_POST['username'] === 'John' && $_POST['password'] === 'Doe') {    
+        // Connect the user
+} else {
+        // Display an error
+        $error = 'Invalid username or password';
+}
+}
 require 'elements/header.php';
 ?>
 <form action="" method="post">
     <div class="form-group">
-        <input type="text" name="username" placeholder="Enter your username">
+        <input class="form-control" type="text" name="username" placeholder="Enter your username">
     </div>
     <div class="form-group">
-        <input type="password" name="password" placeholder="Enter your password">
+        <input class="form-control" type="password" name="password" placeholder="Enter your password">
     </div>
     <button type="submit" class="btn btn-primary">Connexion</button>
 </form>
