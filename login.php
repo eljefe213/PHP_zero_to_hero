15f2a347fe22ad253eebe3c762eb16c9b3279fd9
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-09-28 07:58:43 +0200

    WIP: Update login.php 'Display the alert for the user'

--- login.php
+++ login.php
@@ -10,6 +10,11 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
 }
 require 'elements/header.php';
 ?>
+<?php if ($error): ?>
+    <div class="alert alert-danger">
+        <?= $error; ?>
+    </div>
+<?php endif; ?>
 <form action="" method="post">
     <div class="form-group">
         <input class="form-control" type="text" name="username" placeholder="Enter your username">
