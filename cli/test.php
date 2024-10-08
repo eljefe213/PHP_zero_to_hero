8e5c22dbae9c2c04e64e706c7b5cce52647b0dac
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-08 14:20:35 +0200

    WIP: Update test.php 'calculate the difference between two dates'

--- cli/test.php
+++ cli/test.php
@@ -4,8 +4,8 @@ $date2 = '2024-04-01';
 
 $d = new DateTime($date);
 $d2 = new DateTime($date2);
-$days = $d->diff($d2, true)->days;
-echo "there is $days days between " . $date . " and " . $date2 . "\n";
+$diff = $d->diff($d2, true);
+echo "There is {$diff->y} years, {$diff->m} months and {$diff->d} days between " . $date . " and " . $date2 . "\n";
 
 echo "\n";
 
