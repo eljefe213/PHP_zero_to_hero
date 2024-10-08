969f4733326ddd06815e5420ffeba4c06ea7bcae
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-08 14:57:12 +0200

    WIP: Update test.php 'Add DateInterval'

--- cli/test.php
+++ cli/test.php
@@ -1,15 +1,5 @@
 <?php
-$date = '2024-01-01';
-$date2 = '2024-04-01';
-
-$d = new DateTime($date);
-$d2 = new DateTime($date2);
-$diff = $d->diff($d2, true);
-echo "There is {$diff->y} years, {$diff->m} months and {$diff->d} days between " . $date . " and " . $date2 . "\n";
-
-echo "\n";
-
-$time = strtotime($date);
-$time2 = strtotime($date2);
-$days = floor(abs(($time - $time2)) / (60 * 60 * 24));
-echo "there is " . $days . " days between " . $date . " and " . $date2 . "\n";
+$date = new DateTime('2024-01-01');
+$interval = new DateInterval('P1M1DT1M');
+$date->add($interval);
+var_dump($date);
