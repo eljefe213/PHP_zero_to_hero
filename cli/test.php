0042177fb4d9e75340f1c45124a42afa926db1b2
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-09 12:13:29 +0200

    WIP: Test the method includesHour

--- cli/test.php
+++ cli/test.php
@@ -2,4 +2,7 @@
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Slot.php';
 $slots = new Slot(9, 17);
 $slots2 = new Slot(14, 16);
-var_dump($slots, $slots2);
+var_dump(
+    $slots->includesHour(10),
+    $slots2->includesHour(10)
+);
