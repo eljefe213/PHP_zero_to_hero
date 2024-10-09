8d9f79cd2752aeb4282b9016c2215835ff2fcd8d
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-09 11:37:40 +0200

    WIP: Update class Slot to add the constructor

--- class/Slot.php
+++ class/Slot.php
@@ -2,6 +2,14 @@
 
 class Slot
 {
+
     public $start;
+
     public $end;
+
+    public function __construct(int $start, int $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
 }
--- cli/test.php
+++ cli/test.php
@@ -1,8 +1,5 @@
 <?php
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Slot.php';
-$slots = new Slot();
-$slots->start = 9;
-$slots->end = 17;
-$slots2 = new Slot();
-var_dump($slots);
-var_dump($slots2);
+$slots = new Slot(9, 17);
+$slots2 = new Slot(14, 16);
+var_dump($slots, $slots2);
