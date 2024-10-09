aed1688bcfc51097af23830e6f723606750b16fe
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-09 15:11:08 +0200

    WIP: Create the checkbox method in the class Form.php

--- class/Form.php
+++ class/Form.php
@@ -1,3 +1,15 @@
 <?php
 
-class Form {}
+class Form
+{
+    function checkbox(string $name, string $value, array $data): string
+    {
+        $attribute = '';
+        if (isset($data[$name]) && in_array($value, $data[$name])) {
+            $attribute .= 'checked';
+        }
+        return <<<HTML
+        <input type="checkbox" name="{$name}[]" value="$value">
+HTML;
+    }
+}
