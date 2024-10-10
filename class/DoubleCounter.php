e4b8d71e5052e93333267b92269ba3edd02d1eaf
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-10 13:29:34 +0200

    WIP: Update the DoubleCounter class to extends from the Counter class

--- class/DoubleCounter.php
+++ class/DoubleCounter.php
@@ -1,29 +1,12 @@
 <?php
-
-class DoubleCounter
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'Counter.php';
+class DoubleCounter extends Counter
 {
-    public $file;
-
-    public function __construct(string $file)
-    {
-        $this->file = $file;
-    }
-
-    public function increment(): void
-    {
-        $counter = 1;
-        if (file_exists($this->file)) {
-            $counter = (int)file_get_contents($this->file);
-            $counter++;
-        }
-        file_put_contents($this->file, $counter);
-    }
-
     public function get(): int
     {
         if (!file_exists($this->file)) {
             return 0;
         }
-        return 2 * (int)file_get_contents($this->file);
+        return (int)file_get_contents($this->file) * 2;
     }
 }
