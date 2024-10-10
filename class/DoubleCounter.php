b2d361681fdb8a5ac6fb76f40cd9ab78b0770531
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-10 12:06:21 +0200

    WIP: Create the class DoubleCounter

---
+++ class/DoubleCounter.php
@@ -0,0 +1,29 @@
+<?php
+
+class DoubleCounter
+{
+    public $file;
+
+    public function __construct(string $file)
+    {
+        $this->file = $file;
+    }
+
+    public function increment(): void
+    {
+        $counter = 1;
+        if (file_exists($this->file)) {
+            $counter = (int)file_get_contents($this->file);
+            $counter++;
+        }
+        file_put_contents($this->file, $counter);
+    }
+
+    public function get(): int
+    {
+        if (!file_exists($this->file)) {
+            return 0;
+        }
+        return 2 * (int)file_get_contents($this->file);
+    }
+}
