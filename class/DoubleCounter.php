7ccd9b95a65fa3016bcef446dcd5efb3e94db150
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-10 13:32:21 +0200

    WIP: Update the get method in DoubleCounter class to use the parent get() method in Counter class

--- class/DoubleCounter.php
+++ class/DoubleCounter.php
@@ -4,9 +4,6 @@ class DoubleCounter extends Counter
 {
     public function get(): int
     {
-        if (!file_exists($this->file)) {
-            return 0;
-        }
-        return (int)file_get_contents($this->file) * 2;
+        return parent::get() * 2;
     }
 }
